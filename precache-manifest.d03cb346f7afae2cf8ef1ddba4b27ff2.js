0cc51175ea5adbda02cc6df178f02e6867cf0c87
Dariusz Okonski <darek200180@gmail.com>
2020-02-15 20:53:47 +0100

    Updates

---
+++ precache-manifest.d03cb346f7afae2cf8ef1ddba4b27ff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d50d2f47818bc1b142277281684d23",
+    "url": "/react-favnotes/index.html"
+  },
+  {
+    "revision": "95fc1365051ad216826d",
+    "url": "/react-favnotes/static/js/2.c4405265.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-favnotes/static/js/2.c4405265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cb0d3fa9a6691b09ff",
+    "url": "/react-favnotes/static/js/main.ae3d557d.chunk.js"
+  },
+  {
+    "revision": "3f09e32157c4100ef9b1",
+    "url": "/react-favnotes/static/js/runtime-main.f3bc6131.js"
+  }
+]);
