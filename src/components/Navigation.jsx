ff93ea4ef8e55ee6c1d796c39eb7ba4d94900cb7
Dariusz Okonski <darek200180@gmail.com>
2020-02-15 21:51:27 +0100

    add: component navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav className="navigation">
+      <section className="logo-nav">
+        <h2 className="logo-nav__header">FavNotes</h2>
+        <ul className="logo-nav__items">
+          <li className="logo-nav__item"><NavLink exact to="/">twitters</NavLink></li>
+          <li className="logo-nav__item"><NavLink to="/articles">articles</NavLink></li>
+          <li className="logo-nav__item"><NavLink to='/notes'>notes</NavLink></li>
+        </ul>
+      </section>
+
+
+      <button className="btn">new item</button>
+    </nav>
+  );
+}
+
+export default Navigation;
