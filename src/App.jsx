95af91c5151add2d99cdf462fba1888908573e8f
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:23:23 +0100

    add: display viewContainer at App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation';
 import { BrowserRouter } from 'react-router-dom';
+import ViewContainer from './views/ViewContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navigation />
+
+        <ViewContainer />
       </div>
     </BrowserRouter>
   );
