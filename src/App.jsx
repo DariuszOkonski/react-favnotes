2afcc5ae8e14e90fcf69323abd598e2d1263ba65
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:34:41 +0100

    add: basename parameter to BrowserRouter at App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ViewContainer from './views/ViewContainer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navigation />
 
