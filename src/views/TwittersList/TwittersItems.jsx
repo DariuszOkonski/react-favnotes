428c36b00e96ab7af2e63b34e2450eafadb164cb
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:43:55 +0100

    add: TwittersItems component

---
+++ src/views/TwittersList/TwittersItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TwittersItems = () => {
+  return (
+    <div id="twittersItems">
+      <h1>TwittersItems</h1>
+      <h1>TwittersItems</h1>
+    </div>
+  );
+}
+
+export default TwittersItems;
