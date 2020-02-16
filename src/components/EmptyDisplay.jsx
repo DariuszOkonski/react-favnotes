45d9246b1abd92955c50595cd2ffee5cefbd13b7
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 12:48:40 +0100

    add: EmptyDisplay component

---
+++ src/components/EmptyDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmptyDisplay = () => {
+  return (
+    <div className="empty">
+      <p>There's nothing here yet, please add ome items!</p>
+    </div>
+  );
+}
+
+export default EmptyDisplay;
