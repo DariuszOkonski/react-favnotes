7af0e2d305aebc66ab0d4ad0161ec803cae89f8a
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:22:48 +0100

    add: ViewContainer component

---
+++ src/views/ViewContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ViewContainer = () => {
+  return (
+    <section id="view-container">
+      <h1>ViewContainer</h1>
+    </section>
+  );
+}
+
+export default ViewContainer;
