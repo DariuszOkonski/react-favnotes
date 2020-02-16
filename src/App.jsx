5fd454507165bfc62a6bcc8676366c0aa08d2a72
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 12:47:54 +0100

    add: TwittersContextProvider and wrap ViewContainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import { BrowserRouter } from 'react-router-dom';
 import ViewContainer from './views/ViewContainer';
+import TwittersContextProvider from './contexts/TwittersContext/TwittersContext';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navigation />
+        <TwittersContextProvider>
 
-        <ViewContainer />
+          <ViewContainer />
+
+        </TwittersContextProvider>
       </div>
     </BrowserRouter>
   );
