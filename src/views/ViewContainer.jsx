4f493bbdde5223569e3f67a54fe0c0e99f2ac253
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 13:19:41 +0100

    remove: useContext TwittersContext from VieContainer and move it to TwittersItems

--- src/views/ViewContainer.jsx
+++ src/views/ViewContainer.jsx
@@ -1,15 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import TwittersItems from './TwittersList/TwittersItems';
-import { TwittersContext } from '../contexts/TwittersContext/TwittersContext';
 
 const ViewContainer = () => {
-  const { twitters } = useContext(TwittersContext);
 
   return (
     <section id="view-container">
       <Switch>
-        <Route exact path="/" render={() => <TwittersItems twitters={twitters} />} />
+        <Route exact path="/" render={() => <TwittersItems />} />
         <Route exact path="/articles" render={() => <div>Articles</div>} />
         <Route exact path="/notes" render={() => <div>Notes</div>} />
         <Route render={() => <div>Error</div>} />
