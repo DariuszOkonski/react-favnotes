e4e051c056058c55e672cd0eaf2b66d2f253277e
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 10:10:20 +0100

    add: wrap TwittersItems with TwittersContextProvider

--- src/views/ViewContainer.jsx
+++ src/views/ViewContainer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import TwittersItems from './TwittersList/TwittersItems';
+import TwittersContextProvider from '../contexts/TwittersContext/TwittersContext';
 
 const ViewContainer = () => {
   return (
     <section id="view-container">
       <Switch>
-        <Route exact path="/" component={TwittersItems} />
+        <Route exact path="/" render={() => (
+          <TwittersContextProvider>
+            <TwittersItems />
+          </TwittersContextProvider>
+        )} />
         <Route exact path="/articles" render={() => <div>Articles</div>} />
         <Route exact path="/notes" render={() => <div>Notes</div>} />
         <Route render={() => <div>Error</div>} />
