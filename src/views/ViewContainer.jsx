ff0724738efecf4dbcd4dd57d71de2b5bf48bcbc
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:44:49 +0100

    add TwittersItems to Route "/"

--- src/views/ViewContainer.jsx
+++ src/views/ViewContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import TwittersItems from './TwittersList/TwittersItems';
 
 const ViewContainer = () => {
   return (
     <section id="view-container">
       <Switch>
-        <Route exact path="/" render={() => <div>Twitters</div>} />
+        <Route exact path="/" component={TwittersItems} />
         <Route exact path="/articles" render={() => <div>Articles</div>} />
         <Route exact path="/notes" render={() => <div>Notes</div>} />
         <Route render={() => <div>Error</div>} />
