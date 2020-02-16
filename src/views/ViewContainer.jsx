8b7b67b51825aa226dd9161dd18043117817c712
Dariusz Okonski <darek200180@gmail.com>
2020-02-16 09:32:58 +0100

    add: routing at ViewContainer component

--- src/views/ViewContainer.jsx
+++ src/views/ViewContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 const ViewContainer = () => {
   return (
     <section id="view-container">
-      <h1>ViewContainer</h1>
+      <Switch>
+        <Route exact path="/" render={() => <div>Twitters</div>} />
+        <Route exact path="/articles" render={() => <div>Articles</div>} />
+        <Route exact path="/notes" render={() => <div>Notes</div>} />
+        <Route render={() => <div>Error</div>} />
+      </Switch>
+
     </section>
   );
 }
