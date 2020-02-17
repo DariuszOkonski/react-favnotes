eb2a31d16f2639a4b5b69db9fd888e2f74c8e769
Dariusz Okonski <darek200180@gmail.com>
2020-02-17 09:29:55 +0100

    display: NewItem

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Navigation from './components/Navigation';
 import { BrowserRouter } from 'react-router-dom';
 import ViewContainer from './views/ViewContainer';
 import TwittersContextProvider from './contexts/TwittersContext/TwittersContext';
+import NewItem from './views/NewItem';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navigation />
+
+        <NewItem />
+
         <TwittersContextProvider>
 
           <ViewContainer />
