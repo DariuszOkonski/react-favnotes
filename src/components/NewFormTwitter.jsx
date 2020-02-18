1abbdad689e04d783b3d686be9bf0b9c2f97e9fa
Dariusz Okonski <darek200180@gmail.com>
2020-02-18 09:39:34 +0100

    change: construct NewFromTwitter to keep state and add new Twitt

--- src/components/NewFormTwitter.jsx
+++ src/components/NewFormTwitter.jsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { TwittersContext } from '../contexts/TwittersContext/TwittersContext';
 
-const NewFormTwitter = () => {
+const NewFormTwitter = ({ setFormOff }) => {
+  const { addTwitt } = useContext(TwittersContext);
+
+  const [name, setName] = useState('');
+  const [twitterPage, setTwitterPage] = useState('');
+  const [picture, setPicture] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTwitt(picture, name, description, twitterPage);
+
+    setName('');
+    setTwitterPage('');
+    setPicture('');
+    setDescription('');
+    setFormOff();
+  }
+
   return (
-    <form className="newForm">
-      <input type="text" placeholder="Twitter Name..." required />
+    <form className="newForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Twitter Name..."
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-      <input type="text" placeholder="Twitter Link..." required />
+      <input
+        type="text"
+        placeholder="Twitter Link..."
+        required
+        value={twitterPage}
+        onChange={(e) => setTwitterPage(e.target.value)}
+      />
 
-      <input type="text" placeholder="Image Url..." required />
+      <input
+        type="text"
+        placeholder="Image Url..."
+        required
+        value={picture}
+        onChange={(e) => setPicture(e.target.value)}
+      />
 
-      <textarea placeholder="Description..." required></textarea>
+      <textarea
+        placeholder="Description..."
+        required
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
 
       <button className="btn btn-transparent">Add New Item</button>
     </form>
