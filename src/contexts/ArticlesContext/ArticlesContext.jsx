f808bf164372c1e674cce9a7c69acf39d1c719c8
Dariusz Okonski <darek200180@gmail.com>
2020-02-19 07:25:55 +0100

    add: ArticleContext

---
+++ src/contexts/ArticlesContext/ArticlesContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
+
+export const ArticlesContext = createContext();
+
+const ArticlesContextProvider = (props) => {
+  const [articles, setArticles] = useState([
+    { id: 1, name: 'Dan Abramov', description: 'Working on react.js.', twitterPage: 'https://twitter.com/dan_abramov' },
+    { id: 2, name: 'Ryan Florence', description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.', twitterPage: 'https://twitter.com/ryanflorence' },
+  ]);
+
+  const removeArticle = (id) => {
+    setArticles(articles.filter(article => article.id !== id));
+  }
+
+  const addArticle = (name, description, twitterPage) => {
+    setArticles([...articles, { id: uuid(), name, description, twitterPage }])
+  }
+
+  return (
+    <ArticlesContext.Provider value={{ articles, removeArticle, addArticle }}>
+      {props.children}
+    </ArticlesContext.Provider>
+  );
+}
+
+export default ArticlesContextProvider;
