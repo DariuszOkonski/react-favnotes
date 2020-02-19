b6ee6ba7d736b7fdae2c2a8b70548438a41928d9
Dariusz Okonski <darek200180@gmail.com>
2020-02-19 07:39:37 +0100

    change: code to display all articles from ArticlesContext

--- src/views/ArticlesList/ArticlesItems.jsx
+++ src/views/ArticlesList/ArticlesItems.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ArticlesContext } from '../../contexts/ArticlesContext/ArticlesContext';
+import EmptyDisplay from '../../components/EmptyDisplay';
+import ArticleItem from './ArticleItem';
 
 const ArticlesItems = () => {
-  return (
-    <div>ArticlesItems</div>
-  );
+  const { articles } = useContext(ArticlesContext)
+
+  return articles.length ? (
+    <div id="articlesItems">
+      {
+        articles.map(article =>
+          <ArticleItem key={article.id} {...article} />
+        )
+      }
+    </div>
+  ) : (
+      <EmptyDisplay />
+    );
 }
 
 export default ArticlesItems;
