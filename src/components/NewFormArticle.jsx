6f65ba7765f343232e6c36ab69d15eecf76ebd7b
Dariusz Okonski <darek200180@gmail.com>
2020-02-20 06:33:21 +0100

    add: NewFormArticle logic

--- src/components/NewFormArticle.jsx
+++ src/components/NewFormArticle.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ArticlesContext } from '../contexts/ArticlesContext/ArticlesContext';
+import { AppContext } from '../contexts/AppContext/AppContext';
 
 const NewFormArticle = () => {
+  const { addArticle } = useContext(ArticlesContext);
+  const { setFormOff } = useContext(AppContext)
+
+  const [name, setName] = useState('');
+  const [twitterPage, setTwitterPage] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addArticle(name, description, twitterPage);
+
+    setName('')
+    setTwitterPage('')
+    setDescription('');
+    setFormOff();
+  }
+
   return (
-    <h3>NewFormArticle</h3>
+    <form
+      className="newForm"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Article name..."
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Twitter Link..."
+        required
+        value={twitterPage}
+        onChange={(e) => setTwitterPage(e.target.value)}
+      />
+
+      <textarea
+        placeholder="Description..."
+        required
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+
+      <button className="btn btn-transparent">Ann New Article</button>
+    </form>
   );
 }
 
