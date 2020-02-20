5b02ce72c612649dd7f21fdbbbde76b9a1794c71
Dariusz Okonski <darek200180@gmail.com>
2020-02-20 06:46:32 +0100

    add: NewFormNotes

--- src/components/NewFormNotes.jsx
+++ src/components/NewFormNotes.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { NotesContext } from '../contexts/NotesContext/NotesContext';
+import { AppContext } from '../contexts/AppContext/AppContext';
 
 const NewFormNotes = () => {
+  const { addNote } = useContext(NotesContext);
+  const { setFormOff } = useContext(AppContext);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addNote(name, description);
+
+    setName('');
+    setDescription('');
+    setFormOff();
+  }
   return (
-    <h3>NewFormNotes</h3>
+    <form
+      className="newForm"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Note name..."
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <textarea
+        placeholder="Description..."
+        required
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+
+      <button className="btn btn-transparent">Add New Note</button>
+    </form>
   );
 }
 
