33c672988315af367ff1d68f1df339d0a94cb261
Dariusz Okonski <darek200180@gmail.com>
2020-02-24 07:40:30 +0100

    add: button edit to NoteItem

--- src/views/NotesList/NoteItem.jsx
+++ src/views/NotesList/NoteItem.jsx
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import { NotesContext } from '../../contexts/NotesContext/NotesContext';
+import { AppContext } from '../../contexts/AppContext/AppContext';
 
 const NoteItem = ({ id, name, description }) => {
   const { removeNote } = useContext(NotesContext);
+  const { setFormNoteOn } = useContext(AppContext);
 
   return (
     <div id="noteItem">
       <div className="description">
         <h2 className="header">{name}</h2>
         <p className="paragraph">{description}</p>
+
+        <button onClick={() => setFormNoteOn(id)} className="btn btn-transparent">edit</button>
 
         <button
           onClick={() => removeNote(id)}
