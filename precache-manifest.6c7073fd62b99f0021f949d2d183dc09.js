e96ef0b2110e28cacaebb91017476dc41c9fd3ac
Dariusz Okonski <darek200180@gmail.com>
2020-02-24 08:26:32 +0100

    Updates

---
+++ precache-manifest.6c7073fd62b99f0021f949d2d183dc09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026a856f8d9f0a9a83c4b62246d782d7",
+    "url": "/react-favnotes/index.html"
+  },
+  {
+    "revision": "77b5b76909764d5c6126",
+    "url": "/react-favnotes/static/css/main.78a7a8c9.chunk.css"
+  },
+  {
+    "revision": "4ee559694808d60b4060",
+    "url": "/react-favnotes/static/js/2.ff514c7d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-favnotes/static/js/2.ff514c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b5b76909764d5c6126",
+    "url": "/react-favnotes/static/js/main.93173e60.chunk.js"
+  },
+  {
+    "revision": "3f09e32157c4100ef9b1",
+    "url": "/react-favnotes/static/js/runtime-main.f3bc6131.js"
+  },
+  {
+    "revision": "b993d5cef34c9b0f9744efc9328142b0",
+    "url": "/react-favnotes/static/media/mountain.b993d5ce.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-favnotes/precache-manifest.fb81703dc9f93b36cad7ab1203b18424.js"
+  "/react-favnotes/precache-manifest.6c7073fd62b99f0021f949d2d183dc09.js"
 );
 
 self.addEventListener('message', (event) => {
