270ded9bbe1b36ab1f3a0f5105a6a63e9b3c1947
Dariusz Okonski <darek200180@gmail.com>
2020-02-25 13:42:08 +0100

    add: dummy data to ArticlesContext, NotesContext, TwittersContext

--- src/contexts/ArticlesContext/ArticlesContext.jsx
+++ src/contexts/ArticlesContext/ArticlesContext.jsx
@@ -5,8 +5,8 @@ export const ArticlesContext = createContext();
 
 const ArticlesContextProvider = (props) => {
   const [articles, setArticles] = useState([
-    { id: 1, name: 'Dan Abramov', description: 'Working on react.js.', twitterPage: 'https://twitter.com/dan_abramov' },
-    { id: 2, name: 'Ryan Florence', description: 'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.', twitterPage: 'https://twitter.com/ryanflorence' },
+    { id: 1, name: 'Dan Abramov', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla tenetur voluptas repellendus corrupti laudantium alias cum dolorem, illum perferendis facere officiis at dolorum vero ea laborum et a aperiam doloremque.', twitterPage: 'https://twitter.com/dan_abramov' },
+    { id: 2, name: 'Ryan Florence', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla tenetur voluptas repellendusLorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla tenetur voluptas repellendus corrupti laudantium alias cum dolorem, illum perferendis facere officiis at dolorum vero ea laborum et a aperiam doloremque.corrupti laudantium alias cum dolorem, illum perferendis facere officiis at dolorum vero ea laborum et a aperiam doloremque.', twitterPage: 'https://twitter.com/ryanflorence' },
   ]);
 
   const removeArticle = (id) => {
--- src/contexts/NotesContext/NotesContext.jsx
+++ src/contexts/NotesContext/NotesContext.jsx
@@ -5,10 +5,10 @@ export const NotesContext = createContext();
 
 const NotesContextProvider = (props) => {
   const [notes, setNotes] = useState([
-    { id: 1, name: 'Description 1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellat accusamus labore, dolorem cupiditate reiciendis! Iste delectus iure in exercitationem culpa maiores est dolor quaerat dignissimos, cupiditate obcaecati eos unde.' },
-    { id: 2, name: 'Description 2', description: 'Lorem ipsum dolor sit amet.' },
-    { id: 3, name: 'Description 3', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint minima corrupti cupiditate delectus modi optio.' },
-    { id: 4, name: 'Description 4', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem alias temporibus maiores placeat, quaerat et corporis ipsam porro esse ab.' },
+    { id: 1, name: 'Note 1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellat accusamus labore, dolorem cupiditate reiciendis! Iste delectus iure in exercitationem culpa maiores est dolor quaerat dignissimos, cupiditate obcaecati eos unde.' },
+    { id: 2, name: 'Note 2', description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla tenetur voluptas repellendus corrupti laudantium alias cum dolorem, illum perferendis facere officiis at dolorum vero ea laborum et a aperiam doloremque.' },
+    { id: 3, name: 'Note 3', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint minima Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla tenetur voluptas repellendus corrupti laudantium alias cum dolorem, illum perferendis facere officiis at dolorum vero ea laborum et a aperiam doloremque. corrupti cupiditate delectus modi optio.' },
+    { id: 4, name: 'Note 4', description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem alias temporibus maiores placeat, quaerat et corporis ipsam porro esse ab.' },
   ]);
 
   const removeNote = (id) => {
